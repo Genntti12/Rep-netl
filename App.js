8fa6a6e2fb98c713da22e4e2552ac23c010c17bd
Genntti12 <142169022+Genntti12@users.noreply.github.com>
2025-04-21 11:47:57 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Unified eCandidat App</h1>
+      <p>Use this app to apply to all universities from one place.</p>
+    </div>
+  );
+}
+
+export default App;
